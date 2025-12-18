5a9a091adf140b4e4fd6b8d4da4bb9dc38d6219e
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2025-12-18 16:42:22 +0100

    feat: custom derivative rules. by user
    Signed-off-by: Franz R. Sattler <fsattler@physik.uni-bielefeld.de>

--- modules/FEDeriK/FunctionalD.m
+++ modules/FEDeriK/FunctionalD.m
@@ -1,17 +1,48 @@
-(*FunctionalD*)
+(**********************************************************************************
+    Taking functional derivatives of expressions.
+**********************************************************************************)
 
-(* ::Input::Initialization:: *)
+$userRules = {};
+
+FAddFDRule[obj_, wrt_, res_] :=
+    Module[{},
+        AppendTo[$userRules, {obj, wrt, res}];
+        Print["$userRules is now: ", $userRules];
+    ];
+
+SetAttributes[FAddFDRule, HoldAll];
+
+FClearFDRules[] :=
+    Module[{},
+        $userRules = {};
+    ];
 
 FunctionalD::malformed = "Cannot take a derivative of `1`. Expression is either malformed or this is a bug.";
 
 ClearAll[FunctionalD]
 
+SymmetryFactorsFromList[ex_List] :=
+    Module[{ret},
+        ret = Gather[ex];
+        ret = 1 / Factorial[Length[#]]& /@ ret;
+        Times @@ ret
+    ];
+
 FunctionalD[setup_, expr_, v : (f_[_] | {f_[_], _Integer}).., OptionsPattern[]] :=
     Internal`InheritedBlock[
-        {f, nonConst}
+        {f, nonConst, rule, i}
         ,
+        FunKitDebug[3, "Taking functional derivative of ", expr, " w.r.t. ", {v}];
         Unprotect[f];
         nonConst = DeleteDuplicates @ Sort @ ({f, Power} \[Union] $CorrelationFunctions);
+        (*Apply user-defined rules*)
+        Do[
+            If[MemberQ[rule, f, Infinity],
+                f /: D[rule[[1]], rule[[2]], NonConstants -> nonConst] := Evaluate[rule[[3]]]
+            ]
+            ,
+            {rule, $userRules}
+        ];
         (*Rule for normal functional derivatives*)
         f /: D[f[x_], f[y_], NonConstants -> nonConst] := \[Gamma][{f, f}, {-y, x}];
         (*Rule for normal functional derivatives, but AnyField*)
@@ -35,43 +66,132 @@ FunctionalD[setup_, expr_, v : (f_[_] | {f_[_], _Integer}).., OptionsPattern[]] 
                 FTerm[((-1) FMinus[{a, a}, {id, id}] FMinus[{f, b}, {if, ib}]), Propagator[{b, AnyField}, {ib, ic}], GammaN[{AnyField, f, AnyField}, {-ic, -if, -id}], Propagator[{AnyField, a}, {id, ia}]]
             ];
         (*No derivatives of FTerm, FEx*)
-        f /: D[FTerm[a___], f[y_], NonConstants -> nonConst] :=
-            (
-                Message[FunctionalD::malformed, FTerm[a]];
-                Abort[]
-            );
+        f /: D[FTerm[a___], f[y_], NonConstants -> nonConst] := FTerm[FDOp[f[y]], a];
         f /: D[FEx[a___], f[y_], NonConstants -> nonConst] :=
             (
                 Message[FunctionalD::malformed, FEx[a]];
                 Abort[]
             );
         (*Chain rules*)
-        f /: D[g_[FTerm[a___]], f[y_], NonConstants -> nonConst] := (FTerm[g'[FTerm[a]], FDOp[f[y]], a]);
-        f /: D[Power[FTerm[a___], b_], f[y_], NonConstants -> nonConst] := (FTerm[b, Power[FTerm[a], b - 1], FDOp[f[y]], a]);
-        f /: D[Power[a_, FTerm[b___]], f[y_], NonConstants -> nonConst] := (FTerm[Log[a], Power[a, FTerm[b]], FDOp[f[y]], b]);
+        f /: D[Divide[a_, b_], f[y_], NonConstants -> nonConst] :=
+            Module[{da, db},
+                FunKitDebug[6, "NestRule: Divide"];
+                FunKitDebug[6, "in: ", {a, b}];
+                da = D[a, f[y], NonConstants -> nonConst];
+                db = D[b, f[y], NonConstants -> nonConst];
+                FEx[FTerm[da, b] / b^2, FTerm[-1, a, db] / b^2]
+            ];
+        f /: D[Times[any__], f[y_], NonConstants -> nonConst] /; Length[{any}] > 2 :=
+            Module[{aa},
+                aa = {any};
+                aa =
+                    Flatten @
+                        Map[
+                            If[Head[#] === Times,
+                                List @@ #
+                                ,
+                                #
+                            ]&
+                            ,
+                            aa
+                        ];
+                FunKitDebug[6, "NestRule: ", Times];
+                FEx @@ Table[FTerm[Sequence @@ ReplacePart[aa, i -> D[{aa[[i]]}, f[y], NonConstants -> nonConst]]], {i, 1, Length[aa]}]
+            ];
+        f /: D[g_[FTerm[a___]], f[y_], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: ", g];
+                FTerm[g'[FTerm[a]], FDOp[f[y]], a]
+            );
+        f /: D[Power[FTerm[a___], b_], f[y_], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: Power"];
+                FTerm[b, Power[FTerm[a], b - 1], FDOp[f[y]], a]
+            );
+        f /: D[Power[a_, FTerm[b___]], f[y_], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: Power base"];
+                FTerm[Log[a], Power[a, FTerm[b]], FDOp[f[y]], b]
+            );
         Protect[f];
         D[expr, v, NonConstants -> nonConst]
     ];
 
 FunctionalD[setup_, expr_, v : (f_[_List, _List] | {f_[_List, _List], _Integer}).., OptionsPattern[]] :=
     Internal`InheritedBlock[
-        {f, nonConst}
+        {f, nonConst, rule}
         ,
+        Unprotect[f];
         nonConst = DeleteDuplicates @ Sort @ ({f, Power} \[Union] $CorrelationFunctions);
+        (*Apply user-defined rules*)
+        Do[
+            If[MemberQ[rule, f, Infinity],
+                f /: D[rule[[1]], rule[[2]], NonConstants -> nonConst] := Evaluate[rule[[3]]]
+            ]
+            ,
+            {rule, $userRules}
+        ];
         (*Rule for normal functional derivatives*)
-        f /: D[f[{f1_, f2_}, {i_, j_}], f[{f3_, f4_}, {k_, l_}], NonConstants -> nonConst] := \[Gamma][{f1, f3}, {-k, i}] \[Gamma][{f2, f4}, {-l, j}];
+        f /: D[f[{f1_, f2_}, {i_, j_}], f[{f3_, f4_}, {k_, l_}], NonConstants -> nonConst] := \[Gamma][{f1, f3}, {-k, -i}] \[Gamma][{f2, f4}, {-l, -j}];
+        f /: D[f[{f1__}, {i1__}], f[{f2__}, {i2__}], NonConstants -> nonConst] /; Length[{f1}] == Length[{f2}] === Length[{i1}] === Length[{i2}] :=
+            Module[
+                {n, combis, ret}
+                ,
+                (*construct all combinations of i1, i2*)
+                n = Length[{f1}];
+                Print["NOT YET IMPLEMENTED: FunctionalD for multi-indexed functions"];
+                Abort[];
+            ];
         (*Rule for normal functional derivatives, but AnyField*)
         (*No derivatives of FTerm, FEx*)
-        f /: D[FTerm[a___], f[y_], NonConstants -> nonConst] :=
-            (
-                Message[FunctionalD::malformed, FTerm[a]];
-                Abort[]
-            );
-        f /: D[FEx[a___], f[y_], NonConstants -> nonConst] :=
+        f /: D[FTerm[a___], f[y__], NonConstants -> nonConst] := FTerm[FDOp[f[y]], a];
+        f /: D[FEx[a___], f[y__], NonConstants -> nonConst] :=
             (
                 Message[FunctionalD::malformed, FEx[a]];
                 Abort[]
             );
+        (*Chain rules*)
+        f /: D[Divide[a_, b_], f[y__], NonConstants -> nonConst] :=
+            Module[{da, db},
+                FunKitDebug[6, "NestRule: Divide"];
+                FunKitDebug[6, "in: ", {a, b}];
+                da = D[a, f[y], NonConstants -> nonConst];
+                db = D[b, f[y], NonConstants -> nonConst];
+                FEx[FTerm[da, b] / b^2, FTerm[-1, a, db] / b^2]
+            ];
+        f /: D[Times[any__], f[y__], NonConstants -> nonConst] /; Length[{any}] > 2 :=
+            Module[{aa},
+                aa = {any};
+                aa =
+                    Flatten @
+                        Map[
+                            If[Head[#] === Times,
+                                List @@ #
+                                ,
+                                #
+                            ]&
+                            ,
+                            aa
+                        ];
+                FunKitDebug[6, "NestRule: ", Times];
+                FEx @@ Table[FTerm[Sequence @@ ReplacePart[aa, i -> D[{aa[[i]]}, f[y], NonConstants -> nonConst]]], {i, 1, Length[aa]}]
+            ];
+        f /: D[g_[FTerm[a___]], f[y__], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: ", g];
+                FTerm[g'[FTerm[a]], FDOp[f[y]], a]
+            );
+        f /: D[Power[FTerm[a___], b_], f[y__], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: Power"];
+                FTerm[b, Power[FTerm[a], b - 1], FDOp[f[y]], a]
+            );
+        f /: D[Power[a_, FTerm[b___]], f[y__], NonConstants -> nonConst] :=
+            (
+                FunKitDebug[6, "NestRule: Power base"];
+                FTerm[Log[a], Power[a, FTerm[b]], FDOp[f[y]], b]
+            );
+        Protect[f];
         D[expr, v, NonConstants -> nonConst]
     ];
 
