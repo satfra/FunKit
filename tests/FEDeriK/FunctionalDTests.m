3faeb960c9cd875fcd232930cd47143a6e539993
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-03-05 15:44:46 +0100

    One of the tests was in the wrong file

--- tests/FEDeriK/FunctionalDTests.m
+++ tests/FEDeriK/FunctionalDTests.m
@@ -11,37 +11,24 @@ Import[$FunKitDirectory <> "/tests/boilerplate/setups.m"];
 testSetup = GetFunKitSetupScalar[];
 
 (* Basic field derivative *)
-AppendTo[tests, TestCreate[
-    FunKit`Private`FunctionalD[testSetup, Phi[i], Phi[j]], 
-    \[Gamma][{Phi, Phi}, {-j, i}], 
-    TestID -> "Basic functional derivative"]
-];
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[testSetup, Phi[i], Phi[j]], \[Gamma][{Phi, Phi}, {-j, i}], TestID -> "Basic functional derivative"]];
 
 (* AnyField derivative *)
-AppendTo[tests, TestCreate[
-    FunKit`Private`FunctionalD[testSetup, AnyField[i], Phi[j]], 
-    \[Gamma][{Phi, AnyField}, {-j, i}], 
-    TestID -> "AnyField functional derivative"]
-];
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[testSetup, AnyField[i], Phi[j]], \[Gamma][{Phi, AnyField}, {-j, i}], TestID -> "AnyField functional derivative"]];
 
 (* Derivative of field without indices (should be zero) *)
-AppendTo[tests, TestCreate[
-    FunKit`Private`FunctionalD[testSetup, Phi, Phi[j]], 
-    0, 
-    TestID -> "Derivative of field without indices"]
-];
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[testSetup, Phi, Phi[j]], 0, TestID -> "Derivative of field without indices"]];
 
 (**********************************************************************************
     Correlation Function Derivatives  
 **********************************************************************************)
 
 (* GammaN derivative *)
-AppendTo[tests, TestCreate[
-    FunKit`Private`FunctionalD[testSetup, GammaN[{a, b}, {i, j}], Phi[k]], 
-    GammaN[{Phi, a, b}, {-k, i, j}], 
-    TestID -> "GammaN derivative"]
-];
 
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[testSetup, GammaN[{a, b}, {i, j}], Phi[k]], GammaN[{Phi, a, b}, {-k, i, j}], TestID -> "GammaN derivative"]];
 
 (**********************************************************************************
     Chain Rule Tests
@@ -64,31 +51,29 @@ AppendTo[tests, TestCreate[
 **********************************************************************************)
 
 (* Test that FunctionalD fails appropriately for FEx *)
-AppendTo[tests, TestCreate[
-    CheckAbort[FunKit`Private`FunctionalD[testSetup, FEx[a], Phi[i]], "AbortTriggered"], 
-    "AbortTriggered", 
-    TestID -> "FunctionalD of FEx should abort"]
-];
+
+AppendTo[tests, TestCreate[CheckAbort[FunKit`Private`FunctionalD[testSetup, FEx[a], Phi[i]], "AbortTriggered"], "AbortTriggered", TestID -> "FunctionalD of FEx should abort"]];
 
 (**********************************************************************************
     User-Defined Rules Tests
 **********************************************************************************)
 
 (* Clear any existing user rules *)
+
 FClearFDRules[];
 
 (* Add a custom rule *)
+
 FAddFDRule[customFunc[x_], Phi[y_], customResult];
 
 (* Test custom rule application *)
+
 customResult = FunKit`Private`FunctionalD[testSetup, customFunc[i], Phi[j]];
-AppendTo[tests, TestCreate[
-    customResult, 
-    customResult, 
-    TestID -> "Custom functional derivative rule"]
-];
+
+AppendTo[tests, TestCreate[customResult, customResult, TestID -> "Custom functional derivative rule"]];
 
 (* Clean up user rules *)
+
 FClearFDRules[];
 
 (**********************************************************************************
@@ -96,18 +81,12 @@ FClearFDRules[];
 **********************************************************************************)
 
 (* Test symmetric derivative with matching field counts *)
-AppendTo[tests, TestCreate[
-    Head[FunKit`Private`SymmetricDerivative[{Phi, Psi}, {i, j}, {Phi, Psi}, {k, l}]], 
-    Times, 
-    TestID -> "SymmetricDerivative with matching fields"]
-];
+
+AppendTo[tests, TestCreate[Head[FunKit`Private`SymmetricDerivative[{Phi, Psi}, {i, j}, {Phi, Psi}, {k, l}]], Times, TestID -> "SymmetricDerivative with matching fields"]];
 
 (* Test that SymmetricDerivative fails with mismatched field counts *)
-AppendTo[tests, TestCreate[
-    CheckAbort[FunKit`Private`SymmetricDerivative[{Phi}, {i}, {Phi, Psi}, {j, k}], "AbortTriggered"], 
-    "AbortTriggered", 
-    TestID -> "SymmetricDerivative with mismatched fields should abort"]
-];
+
+AppendTo[tests, TestCreate[CheckAbort[FunKit`Private`SymmetricDerivative[{Phi}, {i}, {Phi, Psi}, {j, k}], "AbortTriggered"], "AbortTriggered", TestID -> "SymmetricDerivative with mismatched fields should abort"]];
 
 (**********************************************************************************
     Multi-Index Functional Derivatives
@@ -116,52 +95,51 @@ AppendTo[tests, TestCreate[
 yukawaSetup = GetFunKitSetupYukawa[];
 
 (* Test derivative with list indices *)
+
 multiIndexResult = FunKit`Private`FunctionalD[yukawaSetup, GammaN[{Phi, Psi}, {i, j}], GammaN[{Phi, Psibar}, {k, l}]];
 
-AppendTo[tests, TestCreate[
-    Head[multiIndexResult], 
-    Times, 
-    TestID -> "Multi-index functional derivative"]
-];
+AppendTo[tests, TestCreate[Head[multiIndexResult], Times, TestID -> "Multi-index functional derivative"]];
 
 (**********************************************************************************
     Nested Function Tests
 **********************************************************************************)
 
 (* Test derivative of function applied to FTerm *)
+
 nestedFuncResult = FunKit`Private`FunctionalD[testSetup, f[FTerm[a]], Phi[i]];
-AppendTo[tests, TestCreate[
-    Head[nestedFuncResult], 
-    FTerm, 
-    TestID -> "Derivative of function applied to FTerm"]
-];
+
+AppendTo[tests, TestCreate[Head[nestedFuncResult], FTerm, TestID -> "Derivative of function applied to FTerm"]];
 
 (* Test derivative with respect to multiple fields *)
+
 multiFieldResult = FunKit`Private`FunctionalD[testSetup, Phi[i] * Phi[j], Phi[k], Phi[l]];
 
-AppendTo[tests, TestCreate[
-    multiFieldResult =!= Undefined, 
-    True, 
-    TestID -> "Multi-field functional derivative"]
-];
+AppendTo[tests, TestCreate[multiFieldResult =!= Undefined, True, TestID -> "Multi-field functional derivative"]];
+
+(**********************************************************************************
+    Source fields in functional derivatives
+**********************************************************************************)
+
+srcSetup = GetFunKitSetupWithSources[];
+
+(* Taking a functional derivative w.r.t. a source field should work *)
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[srcSetup, J[i1], J[i2]], \[Gamma][{J, J}, {-i2, i1}], TestID -> "FunctionalD source: derivative of J w.r.t. J"]];
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[srcSetup, AnyField[i1], J[i2]], \[Gamma][{J, AnyField}, {-i2, i1}], TestID -> "FunctionalD source: derivative of AnyField w.r.t. J"]];
 
 (**********************************************************************************
     Edge Cases and Error Handling
 **********************************************************************************)
 
 (* Test with empty arguments *)
-AppendTo[tests, TestCreate[
-    FunKit`Private`FunctionalD[testSetup, 1, Phi[i]], 
-    0, 
-    TestID -> "Derivative of constant"]
-];
+
+AppendTo[tests, TestCreate[FunKit`Private`FunctionalD[testSetup, 1, Phi[i]], 0, TestID -> "Derivative of constant"]];
 
 (* Test linearity *)
-linearResult1 = FunKit`Private`FunctionalD[testSetup, a*Phi[i] + b*Phi[j], Phi[k]];
-linearResult2 = a*FunKit`Private`FunctionalD[testSetup, Phi[i], Phi[k]] + b*FunKit`Private`FunctionalD[testSetup, Phi[j], Phi[k]];
 
-AppendTo[tests, TestCreate[
-    linearResult1, 
-    linearResult2, 
-    TestID -> "Functional derivative linearity"]
-];
+linearResult1 = FunKit`Private`FunctionalD[testSetup, a * Phi[i] + b * Phi[j], Phi[k]];
+
+linearResult2 = a * FunKit`Private`FunctionalD[testSetup, Phi[i], Phi[k]] + b * FunKit`Private`FunctionalD[testSetup, Phi[j], Phi[k]];
+
+AppendTo[tests, TestCreate[linearResult1, linearResult2, TestID -> "Functional derivative linearity"]];
